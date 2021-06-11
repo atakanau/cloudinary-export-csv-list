3724701d9bb89dcfa5e099c3f03540309884c3b0
atakan au <altun.atakan@gmail.com>
2021-06-11 22:06:56 +0300

    Code update
    • Added support for "raw","image","video" resource types.
    • Code fixed.

--- cloudinary-export-csv.php
+++ cloudinary-export-csv.php
@@ -6,12 +6,11 @@ Download list of all stored resources on Cloudinary as CSV file.
 https://atakanau.blogspot.com/2018/12/cloudinary-yuklu-tum-dosyalar-listeleme.html
 
 */
-	function src_read($output,$next_cursor='',$page=0){
+	function src_read($resource_type,$next_cursor=false,$first=false){
 		$result = new stdClass();
+		$result -> output = '';
+		$result -> first = $first;
 		$output_new = '';
-		$result -> output = $output;
-		$result -> next_cursor = false;
-		$result -> page = $page;
 		$handle = curl_init();
 
 /* 	Replace with your own parameters : 
@@ -19,49 +18,55 @@ https://atakanau.blogspot.com/2018/12/cloudinary-yuklu-tum-dosyalar-listeleme.ht
 		API_SECRET
 		CLOUD_NAME
 		 */
-		$url = 'https://API_KEY:API_SECRET@api.cloudinary.com/v1_1/CLOUD_NAME/resources/image'
+		$url = 'https://API_KEY:API_SECRET@api.cloudinary.com/v1_1/CLOUD_NAME/resources/'.$resource_type
 			.'?max_results=500'
-			.( $next_cursor == '' ? '' : '&next_cursor='.$next_cursor )
+			.( $next_cursor ? '&next_cursor='.$next_cursor : '' )
 			;
 		curl_setopt($handle, CURLOPT_URL, $url);
 		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 		$readed = curl_exec($handle);
 		curl_close($handle);
-
 		$data=json_decode($readed, true);
 
-		if( isset($data['next_cursor']) )
-			$result -> next_cursor = $data['next_cursor'];
+		$result -> next_cursor = isset($data['next_cursor']) ? $data['next_cursor'] : false;
 		
-		if($output=='')
+		if( isset($data['resources']) && count($data['resources']) ){
+			if( $result -> first ){
+				$result -> first = false;
+				foreach($data['resources'] as $rsc){
+					foreach ($rsc as $key => $value){
+						$output_new .= "$key\t";
+					}
+					$output_new .= "\r\n";
+					break;
+				}
+			}
+		
 			foreach($data['resources'] as $rsc){
 				foreach ($rsc as $key => $value){
-					$output_new .= "$key\t";
+					$output_new .= "$value\t";
 				}
 				$output_new .= "\r\n";
-				break;
 			}
-		
-		foreach($data['resources'] as $rsc){
-			foreach ($rsc as $key => $value){
-				$output_new .= "$value\t";
-			}
-			$output_new .= "\r\n";
+			$result -> output = $output_new;
 		}
-		$result -> output .= $output_new;
-		$result -> page += 1;
 		
 		return $result;
 	}
 
 	$output = '';
-	$r = src_read($output);
-	do{
-		$r = src_read($r -> output ,$r -> next_cursor ,$r -> page );
-	}while($r -> next_cursor);
-
+	$first = true;
+	$next_cursor = false;
+	$resource_types = ["raw","image","video"];
+	foreach($resource_types as $resource_type){
+		do{
+			$r = src_read($resource_type,$next_cursor,$first);
+			$output .= $r -> output;
+			$first = $r-> first;
+		}while($next_cursor = $r -> next_cursor);
+	}
 	header("Content-type: application/octet-stream");
 	header("Content-Disposition: attachment; filename=\"cloudinary-all-resources-list.csv\"");
-	echo $r -> output;
+	echo $output;
 
 ?>
